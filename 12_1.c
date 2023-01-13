50a4b9ee492117d70605a72c2e2f0264f6d8b9fc
yumiKKKK <121678395+yumiKKKK@users.noreply.github.com>
2023-01-13 19:23:26 +0900

    Add files via upload

---
+++ 12_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int num1 = 10, num2 = 20;
+	int* ptr1 = &num1, * ptr2 = &num2;
+	*ptr1 += 10;
+	*ptr2 -= 10;
+	ptr1 = &num2;
+	ptr2 = &num1;
+	printf("ptr1가 가리키는 변수에 저장된 값: %d\nptr2가 가리키는 변수에 저장된 값:%d\n", *ptr1, *ptr2);
+	return 0;
+}
